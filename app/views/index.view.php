dafb8d83f351be5f4846871360526e4cb1529ec7
Dripsters123 <ipb22.k.skrastins@vtdt.edu.lv>
2024-05-13 10:54:26 +0300

    test

---
+++ app/views/index.view.php
@@ -0,0 +1,3 @@
+<?php require "../views/components/head.php" ?>
+<h1 class="mt-10 text-orange-200  bg-black">Hello world!</h1>
+<?php require "../views/components/footer.php" ?>
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    "host" => "localhost",
+    "dbname" => "biblioteka",
+    "user" => "root",
+    "password" => "root",
+    "charset" => "utf8mb4"
+]; 
