5dce2ee47360e868a66247af1e54739fa55f2460
dedlyme <k.vasarajs@gmail.com>
2024-05-14 13:30:30 +0300

    ssss

--- db.sql
+++ db.sql
@@ -1,3 +1,17 @@
+create database USER;
+
+use USER;
+CREATE TABLE `user` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `username` VARCHAR(50) NOT NULL,
+  `gmail` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `reset_token` VARCHAR(255),
+  `reset_token_expiry` DATETIME
+);
+
+
+
 /*
 CREATE TABLE taskManager {
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- routes.php
+++ routes.php
@@ -1,6 +1,10 @@
 <?php 
 return [
     "/" => "../app/controllers/index.php",
+    "/logout" => "../app/controllers/auth/logout.php",
+    "/login" => "../app/controllers/auth/login.php",
+    "/resset" => "../app/controllers/auth/resetpass.php",
+    "/register" => "../app/controllers/auth/register.php",
 
 
 ];
