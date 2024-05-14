cc643bf29667ccdef2f8edfd15482ccf9389fa4d
matissbby <matissrolavs2005@gmail.com>
2024-05-14 12:56:50 +0300

    push

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,3 +1,3 @@
 <?php require "../views/components/head.php" ?>
-<h1 class="mt-10 text-orange-200  bg-black">Hello world!</h1>
+<h1 class="text-orange-200  bg-black">Hello world!</h1>
 <?php require "../views/components/footer.php" ?>
