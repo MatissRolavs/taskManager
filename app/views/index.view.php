4e285939f6d94465e32b48e26a6eeb2223c85264
Eduardsss <126756209+Eduardsss@users.noreply.github.com>
2024-05-20 10:52:56 +0300

    Update index.view.php

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,7 +1,11 @@
 <?php require "../app/views/components/head.php" ?>
+<div>
 <h1 class="text-orange-200  bg-black">This is index ble!</h1>
     <p class="mt-10 text-orange-200"><?= $_SESSION["username"] ?></p>
     <form action="/logout" method="POST">
         <button class="logout-button">Logout</button>
     </form>
-<?php require "../app/views/components/footer.php" ?>
+  <?php require "../app/views/components/footer.php"; ?>
+</div>
+
+
