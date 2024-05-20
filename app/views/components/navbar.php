ce10d071e2ea30565e02eb723009031242d0148a
matissbby <matissrolavs2005@gmail.com>
2024-05-20 10:55:18 +0300

    new navbar
    assa

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -1,15 +1,18 @@
-<nav class="bg-gray-800 py-4">
-  <div class="container mx-auto px-4">
-    <ul class="flex justify-between">
-      <li>
-        <a href="/" class="text-white hover:text-gray-300">Home</a>
-      </li>
-      <li>
-        <a href="/create" class="text-white hover:text-gray-300">Create Task</a>
-      </li>
-      <li>
-        <a href="/calander" class="text-white hover:text-gray-300">Calendar</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<div class="flex">
+  <nav class="bg-gray-800 py-4 w-64 min-h-screen">
+    <div class="px-4">
+      <ul class="space-y-2">
+        <li>
+          <a href="/" class="text-white hover:text-gray-300">Home</a>
+        </li>
+        <li>
+          <a href="/create" class="text-white hover:text-gray-300">Create Task</a>
+        </li>
+        <li>
+          <a href="/calander" class="text-white hover:text-gray-300">Calendar</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  
+
