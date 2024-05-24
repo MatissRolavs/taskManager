0c7a619b7eb16c93d5a89cc206548d3c4954e040
Dripsters123 <129275287+Dripsters123@users.noreply.github.com>
2024-05-24 08:59:56 +0300

    yess

--- app/views/tasks/index.view.php
+++ app/views/tasks/index.view.php
@@ -27,7 +27,7 @@ require "../app/views/components/navbar.php";
     <?php foreach ($tasks as $task) { ?>
       <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-2 mb-4">
         <div class="task-item border border-gray-300 rounded-md p-4 flex flex-col justify-between" style="background-color: <?= $task['completed'] ? 'green' : 'red'; ?>">
-          <h2>Name: <?= $task['title'] ?></h2>
+          <h2>Title: <?= $task['title'] ?></h2>
           <p>Description: <?= $task['description'] ?></p>
           <p>Due Date: <?= $task['due'] ?></p>
           <div class="button-group mt-2 flex justify-between">
