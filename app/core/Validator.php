eedea8ff4abd04a5aac4e641d612c2fe25ce34c1
Eduardsss <126756209+Eduardsss@users.noreply.github.com>
2024-05-31 10:56:26 +0300

    Update Validator.php

--- app/core/Validator.php
+++ app/core/Validator.php
@@ -15,12 +15,12 @@ class Validator
 
     public static function validateEmail($email)
     {
-        // Using PHP filter_var to validate email format
+      
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             return false;
         }
 
-        // Check if email domain is Gmail
+ 
         $domain = substr(strrchr($email, "@"), 1);
         if ($domain !== 'gmail.com') {
             return false;
