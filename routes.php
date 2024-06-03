2ef1372f921b4d800ae994e0ebe6f07a5287b4c6
Dripsters123 <129275287+Dripsters123@users.noreply.github.com>
2024-06-03 13:30:16 +0300

    routes updated

--- routes.php
+++ routes.php
@@ -4,10 +4,6 @@ return [
     "/register" => "../app/controllers/auth/register.php",
     "/login" => "../app/controllers/auth/login.php",
     "/logout" => "../app/controllers/auth/logout.php",
-    "/login" => "../app/controllers/auth/login.php",
-    "/forgot" => "../app/controllers/auth/forgot.php",
-    "/reset" => "../app/controllers/auth/resetpass.php",
-    "/register" => "../app/controllers/auth/register.php",
     "/create" => "../app/controllers/tasks/create.php",
     "/edit" => "../app/controllers/tasks/edit.php",
     "/delete" => "../app/controllers/tasks/delete.php",
@@ -15,7 +11,6 @@ return [
     "/calander" => "../app/controllers/tasks/calander.php",
     "/updateTask" => "../app/controllers/tasks/updateTask.php",
     "/search" => "../app/controllers/tasks/search.php",
-    "/quote" => "../app/controllers/tasks/quote.php",
-    "/profile" => "../app/controllers/tasks/profile.php",
+    "/profile" => "../app/controllers/tasks/profile.php"
 ];
 ?>
